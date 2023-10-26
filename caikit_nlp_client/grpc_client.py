77d8ae8bd4b2ddcb3694a2b31df679a3d9ed937f
Laurentiu Bradin <109964136+z103cb@users.noreply.github.com>
2023-10-26 12:07:56 +0300

    Text prediction test running

--- caikit_nlp_client/grpc_client.py
+++ caikit_nlp_client/grpc_client.py
@@ -1,16 +1,36 @@
-#import alog
+import grpc
+
+from alog import alog
 from .client import CaikitNlpClient
 from .model.text_generation import TextGenerationTask, TextGenerationTaskResult
+from .generated.nlpservice_pb2_grpc import NlpServiceStub
+from .generated.caikit.runtime.Nlp.textgenerationtaskrequest_pb2 import TextGenerationTaskRequest
 
-#print(alog.__file__)
-#log = alog.use_channel("GRPC_CLIENT")
+log = alog.use_channel("GRPC_CLIENT")
 
 
 class GrpcCaikitNlpClient(CaikitNlpClient):
 
-    def create_text_generation_task(self, request: TextGenerationTask) -> TextGenerationTaskResult:
-        #log.Info("Calling CreateTextGenerationTask")
-        if request.model_id == "":
-            raise ValueError ("request must have a moodel id")
-        result = TextGenerationTaskResult("Random text")
-        return result
+    def __init__(self, channel: grpc.Channel) -> None:
+        self.stub = NlpServiceStub(channel)
+
+    def create_text_generation_task(self, task: TextGenerationTask) -> TextGenerationTaskResult:
+        log.info("Calling CreateTextGenerationTask")
+        try:
+            if task.model_id == "":
+                raise ValueError("request must have a model id")
+
+            metadata = [("mm-model-id", task.model_id)]
+
+            request = TextGenerationTaskRequest()
+            request.text = task.text
+            request.preserve_input_text = False
+            request.max_new_tokens = 200
+            request.min_new_tokens = 10
+
+            response = self.stub.TextGenerationTaskPredict(request=request,metadata=metadata)
+            result = TextGenerationTaskResult(response.generated_text)
+            return result
+        except Exception as exc:
+            log.error(f"Caught exception {exc}, rethrowing")
+            raise exc
--- tests/test_grpc_client.py
+++ tests/test_grpc_client.py
@@ -1,18 +1,22 @@
 import pytest
+import grpc
 from caikit_nlp_client.grpc_client import GrpcCaikitNlpClient
 from caikit_nlp_client.model.text_generation import TextGenerationTask
 
 
 @pytest.fixture
 def connected_client():
-    '''Returns returns a grpc client connected to a running server'''
-    return GrpcCaikitNlpClient()
+    """Returns returns a grpc client connected to a running server"""
+    channel = grpc.insecure_channel("localhost:50051")
+    return GrpcCaikitNlpClient(channel)
 
 
 def test_create_text_generation_task(connected_client):
     task_request = TextGenerationTask("foobar", "What does foobar mean?")
     connected_client.create_text_generation_task(task_request)
 
+
 def test_create_text_generation_task_with_no_model_id(connected_client):
-    task_request = TextGenerationTask("", "What does foobar mean?")
-    connected_client.create_text_generation_task(task_request)
+    with pytest.raises(ValueError, match="request must have a model id"):
+        task_request = TextGenerationTask("", "What does foobar mean?")
+        connected_client.create_text_generation_task(task_request)
