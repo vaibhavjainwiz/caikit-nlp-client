2d96c48ea1f9d9e7e6a3227d04fff862b6b6f449
Daniele Trifirò <dtrifiro@redhat.com>
2023-11-02 17:58:48 +0100

    tests: mark http test as xfail

--- tests/test_http_predict.py
+++ tests/test_http_predict.py
@@ -1,14 +1,17 @@
+import pytest
 import requests
 
 
-api_url = "http://localhost:8080/api/v1/flan-t5-small-caikit/task/text-generation"
+@pytest.mark.xfail(reason="Stub test")
+def test_http_endpoint():
+    api_url = "http://localhost:8080/api/v1/flan-t5-small-caikit/task/text-generation"
 
-json_input = {
-    "inputs": "At what temperature does liquid Nitrogen boil?",
-    "parameters": {"max_new_tokens": 200, "min_new_tokens": 10},
-}
-response = requests.post(
-    api_url,
-    json=json_input,
-)
-print("response", response)
+    json_input = {
+        "inputs": "At what temperature does liquid Nitrogen boil?",
+        "parameters": {"max_new_tokens": 200, "min_new_tokens": 10},
+    }
+    response = requests.post(
+        api_url,
+        json=json_input,
+    )
+    assert response.status_code == 200
