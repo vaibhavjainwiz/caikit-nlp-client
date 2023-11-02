c050d5c0476c20421ec37db8e2d557cc16b91aaa
Daniele Trifirò <dtrifiro@redhat.com>
2023-11-02 14:50:14 +0100

    tests: cleanup grpc introspection

--- tests/test_grpc_client_introspection.py
+++ tests/test_grpc_client_introspection.py
@@ -1,11 +1,8 @@
 import pytest
-import alog
+from caikit_nlp_client.grpc_channel import GrpcChannelConfig, make_channel
 from caikit_nlp_client.grpc_client_introspection import GrpcCaikitNlpClientIntrospection
-from caikit_nlp_client.grpc_channel import *
 
-from caikit_nlp_client.model.text_generation import TextGenerationTask
 
-log = alog.use_channel("UNIT_TEST")
 @pytest.fixture
 def connected_client():
     """Returns returns a grpc client connected to a locally running server"""
@@ -14,17 +11,22 @@ def connected_client():
 
 
 def test_generate_text(connected_client):
-    generated_text = connected_client.generate_text("flan-t5-small-caikit", "What does foobar mean?")
-    log.debug(f"result generated text is: {generated_text}")
+    generated_text = connected_client.generate_text(
+        "flan-t5-small-caikit", "What does foobar mean?"
+    )
     assert generated_text == "a symphony"
 
 
 def test_generate_text_with_optional_args(connected_client):
-    generated_text = connected_client.generate_text("flan-t5-small-caikit", "What does foobar mean?",
-                                                    preserve_input_text=False, max_new_tokens=20, 
-                                                    min_new_tokens = 4)
-    log.debug(f"result generated text is: {generated_text}")
+    generated_text = connected_client.generate_text(
+        "flan-t5-small-caikit",
+        "What does foobar mean?",
+        preserve_input_text=False,
+        max_new_tokens=20,
+        min_new_tokens=4,
+    )
     assert generated_text == "a symphony"
+
 
 def test_generate_text_with_no_model_id(connected_client):
     with pytest.raises(ValueError, match="request must have a model id"):
@@ -32,15 +34,18 @@ def test_generate_text_with_no_model_id(connected_client):
 
 
 def test_generate_text_stream(connected_client):
-    results = connected_client.generate_text_stream("flan-t5-small-caikit", "What is the meaning of life?")
+    results = connected_client.generate_text_stream(
+        "flan-t5-small-caikit", "What is the meaning of life?"
+    )
     assert len(results) == 9
-    text = ''.join(map(lambda item: item, results))
-    log.debug(f"The answer to 'What is the meaning of life?` is '{text}'")
+
 
 def test_generate_text_stream_with_optional_args(connected_client):
-    results = connected_client.generate_text_stream("flan-t5-small-caikit", "What is the meaning of life?",
-                                                    preserve_input_text=False, max_new_tokens=20, 
-                                                    min_new_tokens = 4)
+    results = connected_client.generate_text_stream(
+        "flan-t5-small-caikit",
+        "What is the meaning of life?",
+        preserve_input_text=False,
+        max_new_tokens=20,
+        min_new_tokens=4,
+    )
     assert len(results) == 9
-    text = ''.join(map(lambda item: item, results))
-    log.debug(f"The answer to 'What is the meaning of life?` is '{text}'")
